16a253903ddec705797356185352e49892007289
Konstantin Pilyugin <kspilyugin@avito.ru>
2022-11-16 10:06:12 +0300

    test root get redirect

--- express/server.js
+++ express/server.js
@@ -7,8 +7,15 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  const head = { 'Content-Type': 'text/html' };
+
+  if (req.url) {
+    res.writeHead(301, { ...head, Location: req.url || '/' });
+  } else {
+    res.writeHead(200, { ...head });
+    res.write('<h1>No link, no redirect</h1>');
+  }
+  
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
