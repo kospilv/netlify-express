4f9da7bdaf1ae9582265077b68cf812ce6cc80a4
Konstantin Pilyugin <kspilyugin@avito.ru>
2022-11-17 10:17:51 +0300

    test routes

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-const redirectHandler = (req, res) => {
+router.get('/', (req, res) => {
   const head = { 'Content-Type': 'text/html' };
 
   if (req.query.url) {
@@ -18,12 +18,24 @@ const redirectHandler = (req, res) => {
   }
   
   res.end();
-};
-
-router.get('/', redirectHandler);
+};);
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.get('/redirect', redirectHandler);
+router.get('/redirect', (req, res) => {
+  const head = { 'Content-Type': 'text/html' };
+
+  if (req.query.url) {
+    res.writeHead(301, { ...head, Location: req.query.url || '/' });
+  } else {
+    res.writeHead(200, { ...head });
+    res.write('<h1>No link, no redirect</h1>');
+  }
+  
+  res.end();
+});
+
+router.get('/another-one', (req, res) => res.json({ route: req.originalUrl, query: req.query }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
