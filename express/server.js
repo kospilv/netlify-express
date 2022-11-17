cbce250eae1b33e5a4412235f1b95115c228f49c
Konstantin Pilyugin <kspilyugin@avito.ru>
2022-11-17 10:23:02 +0300

    test route

--- express/server.js
+++ express/server.js
@@ -20,6 +20,7 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/test', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 router.get('/redirect', (req, res) => {
   const head = { 'Content-Type': 'text/html' };
