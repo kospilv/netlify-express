38a9cd3ebad858403b3ae4c67b5c7000111d0dc2
Konstantin Pilyugin <kspilyugin@avito.ru>
2022-11-17 10:12:11 +0300

    add redirect route

--- express/server.js
+++ express/server.js
@@ -6,20 +6,24 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
+
+const redirectHandler = (req, res) => {
   const head = { 'Content-Type': 'text/html' };
 
-  if (req.url) {
-    res.writeHead(301, { ...head, Location: req.url || '/' });
+  if (req.query.url) {
+    res.writeHead(301, { ...head, Location: req.query.url || '/' });
   } else {
     res.writeHead(200, { ...head });
     res.write('<h1>No link, no redirect</h1>');
   }
   
   res.end();
-});
+};
+
+router.get('/', redirectHandler);
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/redirect', redirectHandler);
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
